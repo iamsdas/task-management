95b6145afbf358b9af15a167e8f1845b9fa65180
Suryashankar Das <suryashankardas.2002@gmail.com>
2022-01-27 17:43:22 +0530

    make priority un-unique

--- tasks/models.py
+++ tasks/models.py
@@ -8,7 +8,7 @@ class Task(models.Model):
     completed = models.BooleanField(default=False)
     created_date = models.DateTimeField(auto_now=True)
     deleted = models.BooleanField(default=False)
-    priority = models.IntegerField(unique=True)
+    priority = models.IntegerField()
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
