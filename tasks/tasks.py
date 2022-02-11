cf4b5658707887cf5881cd6ef344266559f1b7a6
Suryashankar Das <suryashankardas.2002@gmail.com>
2022-02-11 21:45:57 +0530

    send task status mail to users

--- tasks/tasks.py
+++ tasks/tasks.py
@@ -1,30 +1,34 @@
-import time
+from celery.schedules import crontab
 from django.contrib.auth.models import User
 from django.core.mail import send_mail
-from tasks.models import Task
-from datetime import timedelta
-from celery.decorators import periodic_task
 from task_manager.celery import app
 
+from tasks.models import STATUS_CHOICES, Task
 
-@periodic_task(run_every=timedelta(seconds=10))
-def send_mail_reminder():
-    print("starting to process the mails")
+
+@app.on_after_finalize.connect
+def setup_periodic_tasks(sender, **kwargs):
     for user in User.objects.all():
-        pending_qs = Task.objects.filter(user=user, completed=False, deleted=False)
-        email_content = f"You have {pending_qs.count()} pending tasks"
-        send_mail(
-            "Pending Tasks from task manager",
-            email_content,
-            "taks@taskmanager.org",
-            [user.email],
+        sender.add_periodic_task(
+            crontab(0),
+            send_mail_reminder.s(user.id),
         )
-        print(f"Completed processing for user {user.id}")
 
 
 @app.task
-def test_backgroud_jobs():
-    print("This is from the background")
-    for i in range(10):
-        time.sleep(1)
-        print(i)
+def send_mail_reminder(user):
+    print(f"starting to process mail for user {user}")
+
+    email_content = "Task Status Report:\n"
+    for choice in STATUS_CHOICES:
+        pending_qs = Task.objects.filter(user=user, status=choice[0], deleted=False)
+        email_content += f"- {pending_qs.count()} tasks which are {choice[0]}\n"
+
+    send_mail(
+        "Task Status Report",
+        email_content,
+        "tasks@taskmanager.org",
+        [user.email],
+        fail_silently=False,
+    )
+    print(f"Completed processing for user {user}")
